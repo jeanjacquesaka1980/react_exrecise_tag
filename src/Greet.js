1f0038f99099fa01b587f0188b479cd05a7718ce
Jeremy <jeremy.bertin76@hotmail.fr>
2019-12-09 21:37:52 +0100

    first commit

---
+++ src/Greet.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+
+function Greet() {
+    return (
+        <div className="Greet">
+            <header>
+                <p>
+                    Hello again !
+                </p>
+            </header>
+        </div>
+    );
+}
+
+
+
+
+
+export default Greet;
